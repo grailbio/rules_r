534749cbc639b23992e1000b0e64850d4037589a
Siddhartha Bagaria <sbagaria@grailbio.com>
2020-02-20 17:47:33 -0800

    rules_r: make flock_test stable
    The current implementation of flock_test is rough, and has
    synchronization issues. The new implementation does synchronization in a
    much better way.
    
    Tested with `--runs_per_test=100`, which was failing before.

--- tests/flock/flock_test.sh
+++ tests/flock/flock_test.sh
@@ -16,40 +16,64 @@
 set -exuo pipefail
 
 tmpdir="$(mktemp -d)"
+marker_failed="${tmpdir}/failed"
+marker_1="${tmpdir}/1"
+marker_2="${tmpdir}/2"
+marker_3="${tmpdir}/3"
+lock_1="${tmpdir}/lock1"
+lock_2="${tmpdir}/lock2"
+
+mkdir -p "${tmpdir}"
+cleanup() {
+  rm -r "${tmpdir}"
+}
+trap 'cleanup' INT HUP QUIT TERM EXIT
 
 fail() {
   >&2 echo "$@"
+  touch "${marker_failed}"
   exit 1
 }
 
+mark() {
+  touch "$1"
+}
+
+wait_on_marker() {
+  while ! test -e "$1"; do
+    sleep 0.01
+  done
+}
+
 pids=()
-# Acquire a lock in a subshell for some time.
+# Acquire a lock on the first file in a subshell for some time.
 (
-  exec 200>"${tmpdir}/1"
+  exec 200>"${lock_1}"
   external/com_grail_rules_r/R/scripts/flock 200 || fail "unable to acquire lock 1 in proc 1"
-  sleep 1
+  mark "${marker_1}"
+  wait_on_marker "${marker_2}"  # Wait until the second subshell has tried acquiring the lock.
+  exec 200>&- # Close the file descriptor; releasing the lock.
+  mark "${marker_3}"
 ) &
 pids+=($!)
 
 # Try to acquire a lock on the first file, fail on first attempt, and then succeed after waiting.
 (
-  exec 200>"${tmpdir}/1"
+  wait_on_marker "${marker_1}"  # Wait until the first subshell has acquired the lock.
+  exec 200>"${lock_1}"
   external/com_grail_rules_r/R/scripts/flock 200 && fail "should not have acquired lock 1 in proc 2"
-  sleep 1.5
+  mark "${marker_2}"
+  wait_on_marker "${marker_3}"
   external/com_grail_rules_r/R/scripts/flock 200 || fail "unable to acquire lock 1 in proc 2"
 ) &
 pids+=($!)
 
 # Try to acquire a lock on a different file, and succeed immediately.
 (
-  exec 200>"${tmpdir}/2"
+  exec 200>"${lock_2}"
   external/com_grail_rules_r/R/scripts/flock 200 || fail "unable to acquire lock 2 in proc 3"
-  sleep 2
 ) &
 pids+=($!)
 
-# The sleep times in all subshells guarantee this order of completion.
-for pid in "${pids[@]}"; do
-  # This will fail if the subshell failed.
-  wait "${pid}"
-done
+wait # Wait on all children; return code is always 0.
+! test -e "${marker_failed}"

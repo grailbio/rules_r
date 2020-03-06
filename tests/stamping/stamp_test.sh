grailbio/rules_r

commit 8a4ab37560a197e9401fb561bdf9aa0c9cd61269

rules_r: stamp attribute for r_binary and r_test
This stamp attribute will determine whether we depend on the stable
status file in the runfiles of the binary/test. The volatile status file
is always available. This will help people get better cache hits across
builds when their workspace status command is outputting STABLE_ vars
but an individual test does not really need these vars.

NOTE: This is a breaking change for people who were depending on the
stable status file in their r_test or r_markdown target. This file will
not be available by default because the attribute value is False by
default. Volatile status files will continue to be available.

See #37 for more context.

We expect the usage of this attribute to be limited, and we expect
cache hits in stamping to be governed mostly through the management of
the content of stable status file.

The logic extends to r_markdown.

Co-authored-by: forestfang-stripe <45770568+forestfang-stripe@users.noreply.github.com>

tests/stamping/nostamp_test.sh
@@ -1,0 +1,29 @@
+#!/bin/bash
+# Copyright 2020 The Bazel Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -euo pipefail
+
+fail() {
+  echo "$@"
+  exit 1
+}
+
+if ! grep "^VAR" "volatile-status.txt"; then
+  fail "volatile status file expected"
+fi
+
+if [[ -e stable-info.txt ]]; then
+  fail "stable status file not expected"
+fi

tests/stamping/stamp_test.sh
@@ -1,0 +1,30 @@
+#!/bin/bash
+# Copyright 2020 The Bazel Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+set -euo pipefail
+
+fail() {
+  echo "$@"
+  exit 1
+}
+
+if ! grep "^VAR" "volatile-status.txt"; then
+  fail "volatile status file expected"
+fi
+
+if ! grep "^STABLE_VAR" "stable-status.txt"; then
+  fail "stable status file expected and not empty"
+fi

R/internal/binary.bzl
@@ -103,7 +103,9 @@ def _r_binary_impl(ctx):
     )
 
     layered_lib_files = _layer_library_deps(ctx, library_deps)
-    stamp_files = [ctx.info_file, ctx.version_file]
+    stamp_files = [ctx.version_file]
+    if ctx.attr.stamp:
+        stamp_files.append(ctx.info_file)
     runfiles = ctx.runfiles(
         files = library_deps.lib_dirs + stamp_files,
         transitive_files = depset(transitive = [srcs, exe, tools]),
@@ -158,6 +160,11 @@ _R_BINARY_ATTRS = {
     ),
     "script_args": attr.string_list(
         doc = "A list of arguments to pass to the src script",
+    ),
+    "stamp": attr.bool(
+        doc = ("Include the stable status file in the runfiles of the binary. " +
+               "The volatile status file is always included."),
+        default = False,
     ),
     "_binary_sh_tpl": attr.label(
         allow_single_file = True,

tests/.bazelrc
@@ -1,0 +1,1 @@
+build --workspace_status_command=stamping/workspace_status.sh

tests/stamping/BUILD
@@ -1,0 +1,26 @@
+# Copyright 2020 The Bazel Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+load("@com_grail_rules_r//R:defs.bzl", "r_test")
+
+r_test(
+    name = "stamp_test",
+    src = "stamp_test.sh",
+    stamp = True,
+)
+
+r_test(
+    name = "nostamp_test",
+    src = "nostamp_test.sh",
+)

tests/stamping/workspace_status.sh
@@ -1,0 +1,17 @@
+#!/bin/bash
+# Copyright 2020 The Bazel Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+echo "VAR foo"
+echo "STABLE_VAR bar"

commit 7a0dc43dbbe76fe1acbbb79e9c00d12541fef25e

rules_r: fix exporting status vars as env vars in r_binary
This was broken by 8a4ab37560a197e9401fb561bdf9aa0c9cd61269 in which we
stopped exporting stable status var by default.

R/scripts/binary.sh.tpl
@@ -21,9 +21,17 @@ fatal() {
 }
 
 # Make bazel status variables available as environment variables, if available.
-if [[ -e ./stable-status.txt ]]; then
-  e="$(cat ./{stable,volatile}-status.txt | sed -e 's/\([^[:space:]]*\) \(.*\)/export \1=\"\2\"/')"
+status_to_env() {
+  local status_file="$1"
+  e="$(cat "${status_file}" | sed -e 's/\([^[:space:]]*\) \(.*\)/export \1=\"\2\"/')"
   eval "${e}"
+}
+
+if [[ -e "./volatile-status.txt" ]]; then
+  status_to_env "./volatile-status.txt"
+fi
+if [[ -e "./stable-status.txt" ]]; then
+  status_to_env "./stable-status.txt"
 fi
 
 # Export environment variables, if any.

tests/stamping/nostamp_test.sh
@@ -23,7 +23,13 @@ fail() {
 if ! grep "^VAR" "volatile-status.txt"; then
   fail "volatile status file expected"
 fi
+if ! [[ "${VAR:-}" ]]; then
+  fail "volatile status var expected as env var"
+fi
 
 if [[ -e stable-info.txt ]]; then
   fail "stable status file not expected"
+fi
+if [[ "${STABLE_VAR:-}" ]]; then
+  fail "stable status var not expected as env var"
 fi

tests/stamping/stamp_test.sh
@@ -23,7 +23,13 @@ fail() {
 if ! grep "^VAR" "volatile-status.txt"; then
   fail "volatile status file expected"
 fi
+if ! [[ "${VAR:-}" ]]; then
+  fail "volatile status var expected as env var"
+fi
 
 if ! grep "^STABLE_VAR" "stable-status.txt"; then
   fail "stable status file expected and not empty"
+fi
+if ! [[ "${STABLE_VAR:-}" ]]; then
+  fail "stable status var expected as env var"
 fi